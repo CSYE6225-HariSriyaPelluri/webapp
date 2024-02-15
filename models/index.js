f14a02345198faabda0f76f988e9a2978a2c6a8f
Hari Sriya Pelluri <pelluri.h@northeastern.edu>
2024-02-14 21:56:30 -0500

    Removing unwanted code

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const dbSetup = require('../config/db.config')
 const Sequelize = require('sequelize')
-console.log(dbSetup);
+
 const sequelize = new Sequelize(dbSetup.DATABASE, dbSetup.USER, dbSetup.PASSWORD, {
     host: dbSetup.HOST,
     dialect: dbSetup.DIALECT
