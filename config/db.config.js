6d1f76df6474ce463728977b8d79b9addaed8c53
Hari Sriya Pelluri <pelluri.h@northeastern.edu>
2024-02-14 21:21:40 -0500

    Integration Tests added

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const dotenv= require('dotenv');
 dotenv.config();
 //Reference used: https://sequelize.org/docs/v6/getting-started/
 module.exports = {
-    HOST: 'localhost',
+    HOST: process.env.HOST,
     USER: process.env.USER,
     PASSWORD: process.env.PASSWORD,
     DATABASE: process.env.DATABASE,
