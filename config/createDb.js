a4d2b3389d17f5fcce49feecdf430a9997b73ad1
Hari Sriya Pelluri <harisriya03@gmail.com>
2024-03-17 02:09:53 -0400

    ops test

--- config/createDb.js
+++ config/createDb.js
@@ -1,5 +1,6 @@
 const dbSetup = require('./db.config');
 const { Sequelize } = require('sequelize');
+const logger = require('../util/logger');
 async function createDb() {
     const sequelize = new Sequelize('', dbSetup.USER, dbSetup.PASSWORD, {
         host: dbSetup.HOST,
@@ -11,6 +12,7 @@ async function createDb() {
         await sequelize.query(`CREATE DATABASE IF NOT EXISTS \`${dbSetup.DATABASE}\`;`);
         console.log(`Database ${dbSetup.DATABASE} is ready.`);
     } catch (error) {
+
         console.error('Unable to create the database:', error);
         process.exit(1);
     } finally {
