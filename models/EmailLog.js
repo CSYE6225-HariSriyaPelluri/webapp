023a6a0cab88013e742c8d19c7186ea83319a72a
Hari Sriya Pelluri <harisriya03@gmail.com>
2024-03-26 13:35:10 -0400

    Added primary key

--- models/EmailLog.js
+++ models/EmailLog.js
@@ -3,17 +3,15 @@ const sequelize = require('./index');
 const EmailLog = sequelize.define('EmailLog', {
     email: {
       type: DataTypes.STRING,
+      primaryKey: true,
       allowNull: false,
     },
     email_sent:{
         type:DataTypes.DATE,
         defaultValue: DataTypes.NOW
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-      },
+    }
+  }, {
+    timestamps: false,
   });
 
   module.exports=EmailLog
